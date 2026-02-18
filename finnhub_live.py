503c019c4aa69d4ad8e7bcd15ba9a6536ec11c36
Aman Mishra <amanmishra971194@gmail.com>
2026-02-18 20:23:26 +0530

    Set Railway port to 8080

--- finnhub_live.py
+++ finnhub_live.py
@@ -118,6 +118,6 @@ if __name__ == "__main__":
     import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
